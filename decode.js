0a81b8af6a6a09052c8302e4230b6a5003bde476
Climax708 <soryy708@gmail.com>
2021-09-04 02:48:53 +0300

    Change the way dashes are decoded There can be only one continuous dash substring per line

--- decode.js
+++ decode.js
@@ -4,6 +4,7 @@ function decode(data) {
     const decodedBmp = bmp.decode(data);
     const { width, height } = decodedBmp;
     const decodedData = decodedBmp.getData();
+
     const charTable = [];
     for (let i = 0; i < width * height; ++i) {
         const column = i % width;
@@ -14,8 +15,6 @@ function decode(data) {
         const mappedValue = (() => {
             if (byte === 0x00) {
                 return '##';
-            } else if (byte === 0xFF) {
-                return '--';
             } else {
                 if (byte <= 0x0F) {
                     return '0' + Number(byte).toString(16).toUpperCase();
@@ -26,8 +25,26 @@ function decode(data) {
         })();
         charTable[charTable.length - 1].push(mappedValue);
     }
+
     const mappedData = charTable.map(row => row.join(''));
-    return mappedData;
+
+    // The game has some sort of compression that requires the longest continuous substring of `FF`s to be turned in to `--`s
+    // There can be only one continuous `--` substring per line
+    const optimizedData = mappedData.map(line => {
+        const matches = line.match(/F+/gu);
+        if (matches.length === 0) {
+            return line;
+        }
+        const sortedMatches = matches.sort((a, b) => b.length - a.length);
+        const longestMatch = sortedMatches[0];
+        const index = line.indexOf(longestMatch);
+        return [
+            line.slice(0, index),
+            '-'.repeat(longestMatch.length),
+            line.slice(index + longestMatch.length),
+        ].join('');
+    });
+    return optimizedData;
 }
 
 module.exports = decode;
