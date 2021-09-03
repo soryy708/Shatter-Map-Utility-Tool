ac697716df5916f0413b2031d583033089499151
Climax708 <soryy708@gmail.com>
2021-09-04 01:13:18 +0300

    Make each pixel correspond to 2 characters instead of 1

--- decode.js
+++ decode.js
@@ -12,13 +12,16 @@ function decode(data) {
             charTable.push([]);
         }
         const mappedValue = (() => {
-            if (byte === 0) {
-                return '0'; // Should be # but CodeWalker chokes on it
-            } else if (byte === 255) {
-                return '-';
+            if (byte === 0x00) {
+                return '##';
+            } else if (byte === 0xFF) {
+                return '--';
             } else {
-                const possibleChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-                return possibleChars[Math.floor(byte / possibleChars.length)]
+                if (byte <= 0x0F) {
+                    return '0' + Number(byte).toString(16).toUpperCase();
+                } else {
+                    return Number(byte).toString(16).toUpperCase();
+                }
             }
         })();
         charTable[charTable.length - 1].push(mappedValue);
--- encode.js
+++ encode.js
@@ -1,28 +1,35 @@
 const bmp = require('bmp-js');
 
+function chunk(arr) {
+    return arr.reduce(function (result, value, index, array) {
+        if (index % 2 === 0) {
+            result.push(array.slice(index, index + 2));
+        }
+        return result;
+    }, []);
+}
+
 function encode(data) {
     const mappedData = data
-        .map(line => Array.from(line)
-            .map(char => {
-                if (char === '#') {
+        .map(line => chunk(Array.from(line))
+            .map(chars => {
+                const str = chars.join('');
+                if (str === '##') {
                     return [0, 0, 0, 0];
                 }
-                if (char === '-') {
+                if (str === '--') {
                     return [255, 255, 255, 255];
                 }
-                const possibleChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-                const index = possibleChars.findIndex(ch => ch === char);
-                const value = Math.floor(255 * index / possibleChars.length);
+                const value = parseInt(str, 16);
                 return [value, value, value, value];
             })
             .reduce((prev, cur) => ([...prev, ...cur]))
         )
         .reduce((prev, cur) => ([...prev, ...cur]));
-    
+
     const buf = Buffer.from(mappedData);
-    const bmpData = {data: buf, width: data[0].length, height: data.length};
+    const bmpData = { data: buf, width: data[0].length / 2, height: data.length };
     return bmp.encode(bmpData);
 }
-
 
 module.exports = encode;
