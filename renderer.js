343547f50078aea078ab2c8c32e0294e5dd9c5b8
Climax708 <soryy708@gmail.com>
2021-09-04 03:33:24 +0300

    Add toast messages when things happen

--- renderer.js
+++ renderer.js
@@ -13,6 +13,9 @@ const [
     bmpInputBrowseButton,
     shatterMapOutputElement,
     shatterMapOutputCopyButton,
+    toast,
+    closeToastButton,
+    toastText,
 ] = [
         document.getElementById('bmpOutputPathInput'),
         document.getElementById('bmpOutputPathButton'),
@@ -22,7 +25,44 @@ const [
         document.getElementById('bmpInputButton'),
         document.getElementById('shatterMapOutput'),
         document.getElementById('shatterMapOutputCopyButton'),
+        document.getElementById('toast'),
+        document.getElementById('closeToastButton'),
+        document.getElementById('toastText'),
     ];
+
+let toastDismissTimeout = null;
+
+function dismissToast() {
+    toast.classList.remove('success');
+    toast.classList.remove('error');
+    if (toastDismissTimeout) {
+        clearTimeout(toastDismissTimeout);
+        toastDismissTimeout = null;
+    }
+}
+
+function showToast(type, text) {
+    const desiredClass = (() => {
+        switch (type) {
+            case 'success':
+                return 'success';
+            case 'error':
+                return 'error';
+        }
+    })();
+    toast.classList.remove('success');
+    toast.classList.remove('error');
+    toast.classList.add(desiredClass);
+
+    const toastDismissTime = 5000;
+    if (toastDismissTimeout) {
+        clearTimeout(toastDismissTimeout);
+    }
+    toastDismissTimeout = setTimeout(dismissToast, toastDismissTime);
+    toastText.innerText = text;
+}
+
+closeToastButton.addEventListener('click', () => dismissToast());
 
 bmpOutputPathBrowseButton.addEventListener('click', async () => {
     const { canceled, filePath: selectedPath } = await electron.remote.dialog.showSaveDialog({
@@ -46,6 +86,7 @@ bmpOutputSubmitButton.addEventListener('click', async () => {
     await fs.promises.mkdir(path.dirname(bmpOutputPathInput.value), { recursive: true });
     const encodedData = encode(shatterMapInputElement.value.split('\n'));
     await fs.promises.writeFile(bmpOutputPathInput.value, encodedData.data);
+    showToast('success', `Wrote BMP to ${bmpOutputPathInput.value} successfully.`);
 });
 
 bmpInputBrowseButton.addEventListener('click', async () => {
@@ -62,6 +103,7 @@ bmpInputBrowseButton.addEventListener('click', async () => {
     }
     const selectedPath = selectedPaths[0];
     if (!fs.existsSync(selectedPath)) {
+        showToast('error', `File at ${selectedPath} doesn't exist.`);
         return;
     }
     bmpInputBrowseTextInput.value = selectedPath;
@@ -73,4 +115,10 @@ bmpInputBrowseButton.addEventListener('click', async () => {
 shatterMapOutputCopyButton.addEventListener('click', () => {
     shatterMapOutputElement.select();
     document.execCommand('copy');
+    showToast('success', 'Copied to clipboard');
 });
+
+window.onerror = (errorMessage, _s, _l, _c, error) => {
+    console.error(error.stack);
+    showToast('error', errorMessage);
+};
